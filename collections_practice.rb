1c7ba332b73460f26cb361677e5e3f50593992fe
Courtney Yargo <courtneyyargo@gmail.com>
2020-07-20 00:59:38 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,7 +1,12 @@
 def sort_array_asc(array)
-  array.sort do |left, right|
-    
-  end 
+  array.sort do |a, b|
+  if a == b
+    0
+  elsif a < b
+    -1
+  elsif a > b
+    1
+  end
 end
 
 def sort_array_desc(array)
